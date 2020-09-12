322d19e517cc9086dbebfcd6f48fcce36f3d885a
Yuteng <yutengwu@yahoo.com>
2020-09-12 14:34:06 -0700

    working on insert(T data)

--- BST/main.cpp
+++ BST/main.cpp
@@ -13,13 +13,44 @@ using namespace std;
 template<class T>
 class BstNode
 {
+private:
+    T data;
     BstNode *root, *left, *right;
+    
+public:
+    BstNode();
+    ~BstNode();
+    
+    BstNode<T> *insert(T data);
 };
 
 
 
+template<class T>
+BstNode<T>::BstNode():root(NULL), left(NULL), right(NULL){}
+
+template<class T>
+BstNode<T>::~BstNode()
+{
+    
+}
 
 
+template<class T>
+BstNode<T> *BstNode<T>::insert(T data)
+{
+    if(root == NULL)
+    {
+        root->data = data;
+        root->left = NULL;
+        root->right = NULL;
+    }
+    
+    else if(root->data <= data)
+    {
+        
+    }
+}
 
 
 int main()
