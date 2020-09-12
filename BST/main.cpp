eeabe78e0750e9b1d2a65c6d69af7fd52e5749d9
Yuteng <yutengwu@yahoo.com>
2020-09-12 14:27:04 -0700

    .

---
+++ BST/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  BST
+//
+//  Created by Yuteng Wu on 9/12/20.
+//  Copyright © 2020 Yuteng Wu. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+template<class T>
+class BstNode
+{
+    BstNode *root, *left, *right;
+};
+
+
+
+
+
+
+
+int main()
+{
+    cout << "Hello World\n";
+}
